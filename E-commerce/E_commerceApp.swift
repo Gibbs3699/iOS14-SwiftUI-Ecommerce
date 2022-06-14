0d46ddb558f0e1f7eb64dfb9a25ba703172eba8f
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-06-14 22:38:30 +0700

    message

---
+++ E-commerce/E_commerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_commerceApp.swift
+//  E-commerce
+//
+//  Created by TheGIZzz on 14/6/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct E_commerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
