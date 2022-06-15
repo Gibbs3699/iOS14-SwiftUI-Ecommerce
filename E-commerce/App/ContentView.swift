7c736924fb5b54275b637c67a1cccd26b015e330
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-06-15 12:55:33 +0700

    Create a custom navigation bar with logo

--- E-commerce/App/ContentView.swift
+++ E-commerce/App/ContentView.swift
@@ -11,13 +11,30 @@ struct ContentView: View {
     
     // MARK: - BODY
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 0)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            }
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }
+        .ignoresSafeArea(.all, edges: .top)
+    
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro")
     }
 }
