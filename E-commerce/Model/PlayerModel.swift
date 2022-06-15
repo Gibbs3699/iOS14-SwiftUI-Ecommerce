a26bbad119f471eb96463450be1442e659981249
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-06-15 22:47:56 +0700

    Adding playerModel

---
+++ E-commerce/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  E-commerce
+//
+//  Created by TheGIZzz on 15/6/2565 BE.
+//
+
+import SwiftUI
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
